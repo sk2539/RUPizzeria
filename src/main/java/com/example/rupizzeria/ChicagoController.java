b94fd113aa840367b58d70ddfe396b917cb50890
dhyanashri <dhyanashri.raman@gmail.com>
2024-11-05 22:23:06 -0500

    Added dropdown options for types of pizza in ny pizza and chicago pizza

--- src/main/java/com/example/rupizzeria/ChicagoController.java
+++ src/main/java/com/example/rupizzeria/ChicagoController.java
@@ -1,10 +1,15 @@
 package com.example.rupizzeria;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 
-public class ChicagoController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChicagoController implements Initializable {
 
     @FXML
     private RadioButton large;
@@ -18,4 +23,12 @@ public class ChicagoController {
     @FXML
     private RadioButton small;
 
+    @FXML
+    private ChoiceBox<String> chooseType;
+    
+    private final String[] pizzaTypes = {"Deluxe", "BBQ Chicken", "Meatzza", "Build your own"};
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        chooseType.getItems().addAll(pizzaTypes);
+    }
 }
--- src/main/java/com/example/rupizzeria/NewYorkController.java
+++ src/main/java/com/example/rupizzeria/NewYorkController.java
@@ -1,10 +1,15 @@
 package com.example.rupizzeria;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 
-public class NewYorkController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NewYorkController implements Initializable {
     @FXML
     private RadioButton large;
 
@@ -16,4 +21,13 @@ public class NewYorkController {
 
     @FXML
     private RadioButton small;
+
+    @FXML
+    private ChoiceBox<String> chooseType;
+
+    private final String[] pizzaTypes = {"Deluxe", "BBQ Chicken", "Meatzza", "Build your own"};
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        chooseType.getItems().addAll(pizzaTypes);
+    }
 }
