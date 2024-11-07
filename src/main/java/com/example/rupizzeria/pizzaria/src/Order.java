aeaa0ca4e5c63497af198aaf90848c0cf5f868b3
dhyanashri <dhyanashri.raman@gmail.com>
2024-11-06 23:07:46 -0500

    created some edits to Order

--- src/main/java/com/example/rupizzeria/pizzaria/src/Order.java
+++ src/main/java/com/example/rupizzeria/pizzaria/src/Order.java
@@ -5,4 +5,17 @@ import java.util.ArrayList;
 public class Order {
     private int number; //order number
     private ArrayList<Pizza> pizzas; //can use List<E> instead of ArrayList<E>
+
+    public Order(int number, Pizza pizza){
+        this.number = number;
+        pizzas.add(pizza);
+    }
+
+    public void addPizza(Pizza pizza){
+        pizzas.add(pizza);
+    }
+
+    public void removePizza(int i){
+        pizzas.remove(i);
+    }
 }
