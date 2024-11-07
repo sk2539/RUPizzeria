6fbd843bb59a3ef211641f8921eeff631a9abcb4
sk2539 <sk2539@scarletmail.rutgers.edu>
2024-11-07 14:13:25 -0500

    added onAction for orders placed button

--- src/main/java/com/example/rupizzeria/MainPageController.java
+++ src/main/java/com/example/rupizzeria/MainPageController.java
@@ -68,4 +68,18 @@ public class MainPageController {
         }
     }
 
+    @FXML
+    private void handleOpenOrdersPlacedView() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("ordersplaced-view.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Your Orders Placed");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
