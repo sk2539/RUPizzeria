e5b4926f267a395d4818ef21b71ee26b7a69eb63
sk2539 <sk2539@scarletmail.rutgers.edu>
2024-11-16 13:59:14 -0500

    added BuildYourOwnTest junit

---
+++ src/test/java/com/example/rupizzeria/pizzaria/src/BuildYourOwnTest.java
@@ -0,0 +1,81 @@
+package com.example.rupizzeria.pizzaria.src;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class BuildYourOwnTest {
+
+    @Test
+    public void testSmallPizza() {
+        ChicagoPizza chicagoPizza = new ChicagoPizza();
+        Pizza pizza = chicagoPizza.createBuildYourOwn();
+        pizza.setSize(Size.SMALL);
+        assertEquals(8.99, pizza.price(), 0.01);
+    }
+
+    @Test
+    public void testThreeToppingsSelectedForMedium() {
+        ChicagoPizza chicagoPizza = new ChicagoPizza();
+        Pizza pizza = chicagoPizza.createBuildYourOwn();
+        pizza.setSize(Size.MEDIUM);
+        ArrayList<Topping> toppings = new ArrayList<>();
+        toppings.add(Topping.PEPPERONI);
+        toppings.add(Topping.MUSHROOM);
+        toppings.add(Topping.CHEDDAR);
+        pizza.setToppings(toppings);
+        assertEquals(10.99 + (3 * 1.69), pizza.price(), 0.01);
+    }
+
+    @Test
+    public void testFiveToppingSelectForLarge() {
+        NYPizza nyPizza = new NYPizza();
+        Pizza pizza = nyPizza.createBuildYourOwn();
+        pizza.setSize(Size.LARGE);
+        ArrayList<Topping> toppings = new ArrayList<>();
+        toppings.add(Topping.PEPPERONI);
+        toppings.add(Topping.MUSHROOM);
+        toppings.add(Topping.CHEDDAR);
+        toppings.add(Topping.SAUSAGE);
+        toppings.add(Topping.GREENPEPPER);
+        pizza.setToppings(toppings);
+        assertEquals(12.99 + (5 * 1.69), pizza.price(), 0.01);
+    }
+
+    @Test
+    public void testPriceNoSizeSelected() {
+        NYPizza nyPizza = new NYPizza();
+        Pizza pizza = nyPizza.createBuildYourOwn();
+        assertThrows(NullPointerException.class, pizza::price);
+    }
+
+    @Test
+    public void testPriceSmallPizzaWithMaxToppings() {
+        NYPizza nyPizza = new NYPizza();
+        Pizza pizza = nyPizza.createBuildYourOwn();
+        pizza.setSize(Size.SMALL);
+        ArrayList<Topping> toppings = new ArrayList<>();
+        toppings.add(Topping.PROVOLONE);
+        toppings.add(Topping.GREENPEPPER);
+        toppings.add(Topping.BBQCHICKEN);
+        toppings.add(Topping.MUSHROOM);
+        toppings.add(Topping.SPINACH);
+        toppings.add(Topping.BROCCOLI);
+        toppings.add(Topping.JALAPENO);
+        pizza.setToppings(toppings);
+        assertEquals(8.99 + (7 * 1.69), pizza.price(), 0.01);
+    }
+    @Test
+    public void testPriceMediumPizzaWithOneTopping() {
+        ChicagoPizza chicagoPizza = new ChicagoPizza();
+        Pizza pizza = chicagoPizza.createBuildYourOwn();
+        pizza.setSize(Size.MEDIUM);
+        ArrayList<Topping> toppings = new ArrayList<>();
+        toppings.add(Topping.ONION);
+        pizza.setToppings(toppings);
+        assertEquals(10.99 + 1.69, pizza.price(), 0.01);
+    }
+
+}
